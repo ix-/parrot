071976601717a1e81a95c65adc31a49ed38ad9b0
ix@nawi.at <ix@nawi.at>
2014-02-11 21:30:00 +0100

    main.py: added comments and read conif file

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 #!../bin/python3
 
 from os.path import expanduser
+version = 0.1
 home = "/".join((expanduser("~"), ".parrot/"))
--- src/main.py
+++ src/main.py
@@ -1,39 +1,43 @@
 #!../bin/python3
 
-version = 0.1
+from class_pw import pw
+#load config file
+config = {}
+exec(open("config.py").read(), config)
 
 # Arguments
-from class_pw import pw
 import argparse
 parser = argparse.ArgumentParser()
-gen = parser.add_argument_group("gen")
+gen = parser.add_argument_group("gen")               # arguments related to pw-generation
 gen.add_argument("gen", nargs="?",
                     help="if set, a new password is generated")
-gen.add_argument("name",
+gen.add_argument("name",                            # name of the pw
                  help="provide the name of the password")
 gen.add_argument("-l", "--lenght", dest="lenght",
-                    default=12,
+                    default=12,                     # lenght of the pw
                     help="provide the lenght of the password")
-gen.add_argument("-L", "--lower", dest="low",
+gen.add_argument("-L", "--lower", dest="low",       # omitting lower case characters
                     nargs="?", default=True,
                     help="if set, lower case characters are omitted")
-gen.add_argument("-N", "--numeric", dest="num",
+gen.add_argument("-N", "--numeric", dest="num",     # omitting numeric characters
                     nargs="?", default=True,
                     help="if set, numeric characters are omitted")
-gen.add_argument("-P", "--punctuation", dest="punct",
+gen.add_argument("-P", "--punctuation", dest="punct", # omitting punctuation
                     nargs="?", default=True,
-                    help="if set, lower case characters are omitted")
-gen.add_argument("-U", "--upper", dest="upp",
+                    help="if set, punctuation is omitted")
+gen.add_argument("-U", "--upper", dest="upp",       # omitting upper case characters
                     nargs="?", default=True,
                     help="if set, upper case characters are omitted")
 
-args = parser.parse_args()
-if args.gen:
-    password = pw.gen(lenght=int(args.lenght),
-                      low=bool(args.low), num=bool(args.num),
-                      punct=bool(args.punct), upp=bool(args.upp))
-    name=str(args.name)
-    pw.save(password, name)
-    print(" -> ".join((password, name)))
+args = parser.parse_args()  #store the arguments
+if args.gen:                #if arg gen is given, generate pw with the options:
+    password = pw.gen(lenght=int(args.lenght),  # - lenght of the pw
+                      low=bool(args.low),       # - omit lower case characters
+                      num=bool(args.num),       # - omit numeric characters,
+                      punct=bool(args.punct),   # - omit punctuation
+                      upp=bool(args.upp))       # - omit upper case characters
+    name=str("/".join((config.get('home'), args.name))) # name and file structure ...
+    pw.save(password, name)                     # ...to save pw to
+    print(" -> ".join((password, name)))        # and print to stdout
 else:
     print("no option given")
